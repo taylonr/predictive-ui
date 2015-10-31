f0b9983e138430a3d33c280ec05d7c581620b62d
Nate Taylor <natetaylor@Nates-MacBook-Pro.local>
2015-10-31 09:39:56 -0500

    Wire up server to the database.

--- server.js
+++ server.js
@@ -10,7 +10,14 @@ var port = process.env.PORT || 8080;
 var router = express.Router();
 
 router.get('/', function(req, res){
-	res.json({message: 'api up and running'});
+	var db = require('seraph')({server: 'http://localhost:7474', pass: 'xoZS!lu9%vPDLa4Uq9PbBP0m2'});
+
+	var cypher = 'match (current:State {name: {name}}) -->(next:State) return  next, count(next) as total order by count(next) desc limit {limit}';
+
+	db.query(cypher, {name: req.query.node, limit: parseInt(req.query.limit, 10)}, function(err, result){
+		if (err) console.log(err);
+		res.json(result);
+	});
 });
 
 app.use('/api', router);
